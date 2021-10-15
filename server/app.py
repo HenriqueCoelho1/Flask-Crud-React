2f998dd226ce68b2a43f5a157d5455997b1bc041
HenriqueCoelho1 <alleson_henrique@hotmail.com>
2021-10-14 21:00:10 -0300

    Server set up

---
+++ server/app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET"])
+def index():
+    return {
+        "name": "Testing"
+    }
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
